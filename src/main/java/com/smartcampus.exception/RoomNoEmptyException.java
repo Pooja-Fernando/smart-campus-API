f38568c0ae1b8af7e23f40341e83dee0d714ce85
Pooja-Fernando <poojafernando16@gmail.com>
2026-04-24 08:47:36 +0530

    add exceptions for room with empty

---
+++ src/main/java/com/smartcampus.exception/RoomNoEmptyException.java
@@ -0,0 +1,8 @@
+// RoomNotEmptyException.java
+package com.smartcampus.exception;
+
+public class RoomNotEmptyException extends RuntimeException {
+    public RoomNotEmptyException(String message) {
+        super(message);
+    }
+}
