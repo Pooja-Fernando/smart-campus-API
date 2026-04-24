59b4a8ab76e2ad8a70150f15875189d9917ef563
Pooja-Fernando <poojafernando16@gmail.com>
2026-04-24 08:49:48 +0530

    add exceptions

---
+++ src/main/java/com/smartcampus.exception/SensorUnavailableException.java
@@ -0,0 +1,8 @@
+// SensorUnavailableException.java
+package com.smartcampus.exception;
+
+public class SensorUnavailableException extends RuntimeException {
+    public SensorUnavailableException(String message) {
+        super(message);
+    }
+}
